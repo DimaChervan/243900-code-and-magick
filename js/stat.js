56c5eabbebfba172962424556741db89f10aa4e0
Dmitriy Chervan <dmitriy.chervan@yandex.ru>
2017-05-01 15:27:43 +0300

    Сделал вывод гистограммы во время победы

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,97 @@
 'use strict';
 
-function renderStatistics (ctx, names, times) {
-  /*ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+window.renderStatistics = function (ctx, names, times) {
+  var MAX_TIME = Math.max.apply(null, times);
+  var CLOUD_X = 100;
+  var CLOUD_Y = 10;
+  var CLOUD_HEIGHT = 270;
+  var CLOUD_WIDTH = 420;
+  var CLOUD_COLOR1 = 'rgba(0, 0, 0, 0.7)';
+  var CLOUD_COLOR2 = 'rgba(255, 255, 255, 1)';
+  var SHADOW_OFFSET = 10;
+  var SUCCESS_MESSAGE_TEXT1 = 'Ура, вы победили';
+  var SUCCESS_MESSAGE_TEXT2 = 'Список результатов:';
+  var SUCCESS_MESSAGE_OFFSET_X = 20;
+  var SUCCESS_MESSAGE_OFFSET_Y1 = 30;
+  var SUCCESS_MESSAGE_OFFSET_Y2 = SUCCESS_MESSAGE_OFFSET_Y1 * 1.5;
+  var SUCCESS_MESSAGE_X = CLOUD_X + SUCCESS_MESSAGE_OFFSET_X;
+  var HISTOGRAM_HEIGHT = 150;
+  var BAR_MAX_HEIGHT = HISTOGRAM_HEIGHT / MAX_TIME;
+  var HISTOGRAM_X = 150;
+  var HISTOGRAM_Y = 250;
+  var NAME_OFFSET_Y = 20;
+  var TIME_OFFSET_Y = 30;
+  var CHART_Y = HISTOGRAM_Y - NAME_OFFSET_Y;
+  var CHART_WIDTH = 40;
+  var CHART_OFFSET_X = 50;
+  var CHART_INIT_X = CHART_WIDTH + CHART_OFFSET_X;
+  var TIME_INIT_Y = HISTOGRAM_Y - TIME_OFFSET_Y;
 
-  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.fillRect(100, 10, 420, 270);
+  renderRect(
+      ctx,
+      CLOUD_COLOR1,
+    [
+      CLOUD_X + SHADOW_OFFSET,
+      CLOUD_Y + SHADOW_OFFSET,
+      CLOUD_WIDTH,
+      CLOUD_HEIGHT
+    ]
+  );
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  renderRect(
+      ctx,
+      CLOUD_COLOR2,
+      [CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT]
+  );
 
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(names[i], 150 + 90 * i, 250);
+  renderText(
+      ctx,
+      SUCCESS_MESSAGE_TEXT1,
+      SUCCESS_MESSAGE_X,
+      CLOUD_Y + SUCCESS_MESSAGE_OFFSET_Y1
+  );
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.fillRect(150 + 90 * i, 230, 40, times[i] * (-150 / Math.max.apply(null, times)));*/
-  }
+  renderText(
+      ctx,
+      SUCCESS_MESSAGE_TEXT2,
+      SUCCESS_MESSAGE_X,
+      CLOUD_Y + SUCCESS_MESSAGE_OFFSET_Y2
+  );
+
+  names.forEach(function (name, index) {
+    var chartHeight = times[index] * (-BAR_MAX_HEIGHT);
+    var chartX = HISTOGRAM_X + CHART_INIT_X * index;
+
+    renderText(ctx, times[index].toFixed(0), chartX, TIME_INIT_Y + chartHeight);
+    renderText(ctx, name, chartX, HISTOGRAM_Y);
+    renderRect(ctx, getChartColorByName(name), [chartX, CHART_Y, CHART_WIDTH, chartHeight]);
+  });
+};
+
+function renderRect(ctx, color, coords) {
+  ctx.fillStyle = color;
+  ctx.fillRect.apply(ctx, coords);
+}
+
+function renderText(ctx, text, x, y) {
+  var TEXT_COLOR = 'rgba(0, 0, 0, 1)';
+  var TEXT_FONT = '16px PT Mono';
+
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_FONT;
+  ctx.fillText(text, x, y);
+}
+
+function getChartColorByName(name) {
+  var MY_CHART_COLOR = 'rgba(255, 0, 0, 1)';
+
+  return name === 'Вы' ? MY_CHART_COLOR : getFriendColor();
+}
+
+function getFriendColor() {
+  var maxOpacity = 0.9;
+  var minOpacity = 0.1;
+  var opacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
+
+  return 'rgba(0, 0, 255, ' + opacity + ')';
 }
